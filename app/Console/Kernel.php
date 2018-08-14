3c14042f9298130c2c850e3ed29fff550fc81c2d
uniti-tim <timothy.carambat@uniti.com>
2018-08-14 14:53:53 -0500

    make mailer with simple stats on scheduler job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,8 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+use App\MailReporter;
+
 class Kernel extends ConsoleKernel
 {
     /**
@@ -24,6 +26,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->call(function(){
+          MailReporter::makeMonthlyReport();
+        });
         // $schedule->command('inspire')
         //          ->hourly();
     }
